b5d0cec82111d708a9d6b34b1dc8150f53bcc987
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-09 14:32:21 +0200

    create service files

---
+++ airsen-frontend/src/app/features/forum/services/forum.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Category } from '../models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForumService {
+  private readonly apiUrl = `${environment.apiUrl}/api/v1/forum/categories`;
+    private http = inject(HttpClient);
+    categories: Observable<Category[]> = this.http.get<Category[]>(this.apiUrl);
+
+    getCategories(): Observable<Category[]> {
+      return this.categories;
+    }
+}
