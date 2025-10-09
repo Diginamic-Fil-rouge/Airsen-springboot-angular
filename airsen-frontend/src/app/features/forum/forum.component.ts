63f534c74f143d23ddb4581e71f07870f4ccc6d0
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-09 14:32:54 +0200

    create category component and first integration

--- airsen-frontend/src/app/features/forum/forum.component.ts
+++ airsen-frontend/src/app/features/forum/forum.component.ts
@@ -1,12 +1,21 @@
+import { inject } from '@angular/core';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ForumService } from './services/forum.service';
+import { CategoryComponent } from "./categories/category.component";
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
-  styleUrls: ['./forum.component.scss']
+  styleUrls: ['./forum.component.scss'],
+  imports: [CategoryComponent]
 })
+
 export class ForumComponent {
+
+  private forumService = inject(ForumService);
+  categories$ = this.forumService.getCategories();
+
   constructor(private router: Router) {}
 
   goToHome(): void {
