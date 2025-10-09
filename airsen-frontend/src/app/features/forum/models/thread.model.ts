59dcc2ef6b580120a85d7bd94046787918d4b08a
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-09 12:09:33 +0200

    create forum entities model

---
+++ airsen-frontend/src/app/features/forum/models/thread.model.ts
@@ -0,0 +1,20 @@
+import { User } from "@/app/core/auth/models/user.model"
+import { Category } from "./category.model";
+import { Message } from "./message.model";
+import { Vote } from "./vote.model";
+
+export interface Thread{
+    id: number;
+    title: string;
+    content: string;
+    author: User;
+    category: Category;
+    messages: Message[];
+    votes: Vote[];
+    createdDate: Date;
+    lastMessageDate: Date;
+    viewCount: number;
+    likeCount: number;
+    closed: boolean;
+    pinned: boolean;
+}
