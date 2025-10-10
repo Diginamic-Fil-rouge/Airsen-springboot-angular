2ba2ffeaa30bb176fdee740660fe315b3abef2a7
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-10 15:37:03 +0200

    forum component logic and style

--- airsen-frontend/src/app/features/forum/forum.component.ts
+++ airsen-frontend/src/app/features/forum/forum.component.ts
@@ -1,26 +1,38 @@
 import { inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { ForumService } from './services/forum.service';
+import { ThreadService } from './services/thread.service';
 import { CategoryComponent } from "./categories/category.component";
+import { ThreadComponent } from './threads/thread.component';
+import { Observable } from 'rxjs';
+import { Thread } from './models/thread.model';
+import { Page } from './models/page.model';
 
 @Component({
   standalone: true,
   selector: 'app-forum',
   templateUrl: './forum.component.html',
   styleUrls: ['./forum.component.scss'],
-  imports: [CategoryComponent, CommonModule]
+  imports: [CategoryComponent, ThreadComponent, CommonModule]
 })
 
 export class ForumComponent {
 
   private forumService = inject(ForumService);
+  private threadService = inject(ThreadService);
   categories$ = this.forumService.getCategories();
+  allThreads$ = this.threadService.getAllThreads();
+  threads$ = this.threadService.getThreads(1);
 
-  constructor(private router: Router) {}
+  getThreads(id: number) {
+     this.allThreads$ = new Observable<Page>;
+    this.threads$ = this.threadService.getThreads(id);
 
-  goToHome(): void {
-    this.router.navigate(['/home']);
+  }
+
+  getAllThreads() {
+    this.threads$ = new Observable<Thread[]>;
+    this.allThreads$ = this.threadService.getAllThreads();
   }
 }
