02bbbf39bce40681cf51c0001c1f22219e2d6bc9
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-10 10:44:52 +0200

    implement categories and thread in forum

--- airsen-frontend/src/app/features/forum/categories/category.component.ts
+++ airsen-frontend/src/app/features/forum/categories/category.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Category } from '../models/category.model';
 
 @Component({
+  standalone: true,
   selector: 'forum-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
 
-  category: Category = {
-    id: 0,
-    name: '',
-    description: '',
-    color: '',
-    threads: []
-  };
+  category = input<Category>({
+     id: 0,
+      name: "",
+      description: "",
+      color: "",
+      threads: []
+  },);
 
   constructor(private router: Router) {}
 
