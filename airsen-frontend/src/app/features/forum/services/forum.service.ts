9511e07f6a99d52b805ad3d76dda39acc6390886
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-10 15:36:19 +0200

    implement threadService

--- airsen-frontend/src/app/features/forum/services/forum.service.ts
+++ airsen-frontend/src/app/features/forum/services/forum.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@/environments/environment';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Category } from '../models/category.model';
 
 @Injectable({
