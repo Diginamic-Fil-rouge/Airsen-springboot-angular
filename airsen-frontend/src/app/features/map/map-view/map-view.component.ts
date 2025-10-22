f2228cb351c1013607cb5cdf11dfa2b9c6ab9bee
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-22 16:59:11 +0200

    feat(map): implement map datas logic and render markers on map

--- airsen-frontend/src/app/features/map/map-view/map-view.component.ts
+++ airsen-frontend/src/app/features/map/map-view/map-view.component.ts
@@ -1,5 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, input, inject, Output, EventEmitter } from '@angular/core';
 import * as L from 'leaflet';
+import { Observable } from 'rxjs';
+import { Commune } from '../models/commune.model';
+import { GeographicService } from '../services/geographic.service';
 
 @Component({
   standalone: false,
@@ -9,15 +12,24 @@ import * as L from 'leaflet';
 })
 export class MapViewComponent implements AfterViewInit {
   private map: any;
+  private geographicService = inject(GeographicService)
+
+  // communes$ = this.geographicService.getCommunesWithCoordinates();
+  communes = input<Observable<Commune[]>>();
+  @Output() onMarkerClick = new EventEmitter<any>();
 
   ngAfterViewInit(): void {
     this.initMap();
   }
 
+  ngOnChanges() {
+    this.initMarkers();
+  }
+
   private initMap(): void {
     this.map = L.map('map', {
       // coordinates of the middle of France
-      center: [ 46.3622, 1.5231 ],
+      center: [46.3622, 1.5231],
       zoom: 6
     });
 
@@ -29,4 +41,23 @@ export class MapViewComponent implements AfterViewInit {
 
     tiles.addTo(this.map);
   }
+
+  initMarkers(){
+    this.communes()?.forEach(communes => {
+      console.log("commune : ", communes);
+      communes.forEach(commune => {
+        this.addMarketToMap(commune);
+      });
+    });
+  }
+
+  addMarketToMap(commune: any) {
+    if (!commune.latitude || !commune.longitude) {
+      return;
+    }
+    L.marker([commune.latitude, commune.longitude]).addTo(this.map).on('click', () => {
+      this.onMarkerClick.emit(commune);
+    });
+  }
+
 }
--- airsen-frontend/src/app/features/map/map.component.ts
+++ airsen-frontend/src/app/features/map/map.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AuthService } from '@/auth/services/auth.service';
 import { AuthUser } from '@/auth/models/auth.model';
 import * as L from 'leaflet';
+import { GeographicService } from './services/geographic.service';
+import { Commune } from './models/commune.model';
+import { WeatherService } from './services/weather.service';
+import { Weather } from './models/weather.model';
+import { AirQualityService } from './services/air-quality.service';
+import { AirQuality } from './models/airQuality.model';
 
 @Component({
   standalone: false,
@@ -14,22 +21,109 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit, OnDestroy {
   private authService = inject(AuthService);
+  private geographicService = inject(GeographicService);
+  private weatherService = inject(WeatherService);
+  private airQualityService = inject(AirQualityService);
+
   private router = inject(Router);
 
   currentUser: AuthUser | null = null;
   isLoading = true;
   private destroy$ = new Subject<void>();
 
+  communeClicked: Commune | null = null;
+  airQualityClicked: AirQuality | null = null;
+  weatherClicked: Weather | null = null;
+  communes = new Observable<Commune[]>();
+  weatherDatas: Observable<Weather>[] | null = null;
+  airQualityDatas: Observable<AirQuality>[] | null = null;
+  isLoadingDatas = false;
+
+  dataErrors: string[] | null = null;
+
   ngOnInit(): void {
     this.loadUserData();
     if (!this.currentUser){
       this.router.navigate(['/auth/login']);
     }
+    this.loadMapDatas();
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  loadMapDatas(){
+    this.communes = this.geographicService.getCommunesWithCoordinates();
+
+    this.communes.forEach(communes => {
+      communes.forEach(commune =>
+        this.weatherService.getCurrentWeather(commune.inseeCode).subscribe({ next: (data) => {
+          this.weatherDatas?.push(data);
+        },
+        error: (error) => {
+          console.error('Error loading weather data:', error);
+        }
+      })
+      )
+    });
+
+    this.communes.forEach(communes => {
+      communes.forEach(commune =>
+        this.airQualityService.getAirQuality(commune.inseeCode).subscribe({ next: (data) => {
+          this.airQualityDatas?.push(data);
+        },
+        error: (error) => {
+          console.error('Error loading air quality data:', error);
+        }
+      })
+      )
+    });
+  }
+
+  clickEvent(commune: Commune) {
+    if (this.communeClicked && this.communeClicked.inseeCode === commune.inseeCode) {
+      return;
+    }
+    
+    this.dataErrors = null;
+    this.isLoadingDatas = true;
+
+    if (this.communeClicked) {
+      this.communeClicked = null;
+      this.weatherClicked = null;
+      this.airQualityClicked = null;
+    }
+
+    let weatherLoaded = false;
+    let airQualityLoaded = false;
+
+    this.communeClicked = commune;
+
+    this.weatherDatas?.forEach(datas => {
+      console.log("weather : ", datas);
+      datas.forEach(data => {
+        if (data.communeName === commune.name) {
+          this.weatherClicked = data;
+          weatherLoaded = true;
+        }
+      })
+    });
+
+    this.airQualityDatas?.forEach(datas => {
+      console.log("air quality : ", datas);
+      datas.forEach(data => {
+        if (data.communeInseeCode === commune.inseeCode) {
+          this.airQualityClicked = data;
+          airQualityLoaded = true;
+        }
+      })
+    });
+
+
+    this.isLoadingDatas = false;
+    
   }
 
   /**
