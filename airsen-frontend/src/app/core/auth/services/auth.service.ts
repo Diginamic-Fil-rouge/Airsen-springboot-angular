331ffed7b3e4cf99b01c633b81d8f83d79e9e972
Robin Cassan <robin.cassan@netc.eu>
2025-10-24 12:08:59 +0200

    fix(user_profile): cleaning comments

--- airsen-frontend/src/app/core/auth/services/auth.service.ts
+++ airsen-frontend/src/app/core/auth/services/auth.service.ts
@@ -40,21 +40,6 @@ export class AuthService {
     private storageService: StorageService
   ) {
     this.initializeAuth();
-
-  //   // 🧪 TEST : simuler un utilisateur connecté (temporaire)
-  // // -------------------------------------------------------
-  // setTimeout(() => {
-  //   console.log('🔵 Simulation de connexion...');
-  //   this.isAuthenticatedSubject.next(true);
-  //   this.currentUserSubject.next({
-  //     id: 1,
-  //     firstName: 'Test',
-  //     lastName: 'User',
-  //     email: 'test@airsen.fr',
-  //     role: 'USER'
-  //   });
-  // }, 3000); // après 3 secondes
-
   }
 
   updateProfile(data: any) {
