2f002eddd6c85c63f69c9cebe9d2201007ad6001
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-27 12:36:50 +0100

    feat(mapPopup): add custom component for map popup, button to commune datas anchor in popup now works

--- airsen-frontend/src/app/features/map/map-view/map-view.component.ts
+++ airsen-frontend/src/app/features/map/map-view/map-view.component.ts
@@ -1,9 +1,10 @@
-import { Component, AfterViewInit, input, inject, Output, EventEmitter } from '@angular/core';
+import { Component, AfterViewInit, input, inject, Output, EventEmitter, ViewContainerRef, Injector, ComponentRef, createComponent } from '@angular/core';
 import * as L from 'leaflet';
+import * as I from './icons';
 import { Observable } from 'rxjs';
 import { Commune } from '../models/commune.model';
 import { GeographicService } from '../services/geographic.service';
-
+import { MarkerPopupComponent } from './popup/markerPopup.component';
 @Component({
   standalone: false,
   selector: 'app-map-view',
@@ -17,9 +18,13 @@ export class MapViewComponent implements AfterViewInit {
   markersInitialized = false;
   communes = input<Observable<Commune[]>>();
   communeClicked = input<Commune | null>();
-  markers: L.Marker[] = [];
   @Output() onMarkerClick = new EventEmitter<any>();
   @Output() anchorLinkClicked = new EventEmitter<any>();
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private injector: Injector
+  ) { }
 
   ngAfterViewInit(): void {
     this.initMap();
@@ -65,22 +70,74 @@ export class MapViewComponent implements AfterViewInit {
     if (!commune.latitude || !commune.longitude) {
       return;
     }
-    const icon = L.icon({
-      iconUrl: 'assets/images/marker.png',
-      iconSize: [20, 20],
-      iconAnchor: [10, 5],
-      popupAnchor: [-3, -76]
+
+    let icon: any = null;
+
+    if (commune.qualifier === 'Bon') {
+      icon = I.goodIcon;
+    } else if (commune.qualifier === 'Moyen') {
+      icon = I.mediumIcon;
+    }
+    else if (commune.qualifier === 'Dégradé') {
+      icon = I.deterioratedIcon;
+    }
+    else if (commune.qualifier === 'Mauvais') {
+      icon = I.badIcon;
+    }
+    else if (commune.qualifier === 'Très mauvais') {
+      icon = I.veryBadIcon;
+    }
+    else if (commune.qualifier === 'Extrêmement mauvais') {
+      icon = I.extremelyBadIcon;
+    }
+    else {
+      icon = I.undefinedIcon;
+    }
+
+    const marker = L.marker([commune.latitude, commune.longitude], { icon: icon }).addTo(this.map);
+
+    marker.on('click', () => {
+      this.onMarkerClick.emit(commune);
+      this.openPopup(marker, commune);
+    });
+  }
+
+  openPopup(marker: L.Marker, commune: Commune) {
+    // Create DOM container for popup content
+    const popupContainer = document.createElement('div');
+
+    // Create dynamic Angular component **rendered into** popupContainer
+    const compRef = this.viewContainerRef.createComponent(MarkerPopupComponent, {
+      injector: this.injector,
+    });
+    popupContainer.appendChild(compRef.location.nativeElement);
+
+    // pass inputs
+    compRef.instance.commune = commune;
+
+    // subscribe to output and forward to parent
+    const sub = compRef.instance.more.subscribe(() => {
+      this.anchorLinkClicked.emit(commune);
     });
 
-    this.markers.push(L.marker([commune.latitude, commune.longitude], { icon: icon }).addTo(this.map).on('click', () => {
-      this.onMarkerClick.emit(commune);
+    // open popup
+    var popup = L.popup()
+    .setLatLng([commune.latitude, commune.longitude])
+    .setContent(popupContainer)
+    .openOn(this.map);
 
-      var latlng = L.latLng(commune.latitude, commune.longitude);
-      var popup = L.popup()
-        .setLatLng(latlng)
-        .setContent('<div style="text-align: center; font-weight: bold">' + commune.name + '</div><br>Qualité de l\'air : ' + commune.airQuality + '<br>Température : ' + commune.temperature + ' °C<br><button (click)="anchorLinkClicked.emit()">Plus de details</button>')
-        .openOn(this.map);
-    }));
+    // Destroy dynamic component when popup closes (prevents leaks)
+    const onClose = () => {
+      try {
+        sub.unsubscribe();
+      } catch { }
+      try {
+        compRef.destroy();
+      } catch { }
+      marker.off('popupclose', onClose);
+    };
+
+    marker.on('popupclose', onClose);
   }
 
 }
