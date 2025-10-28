94b28fda3970becfbceb00e9c8b7ad80a3bb047e
Tommy-BOU <tommy.boubaker@free.fr>
2025-10-28 15:52:29 +0100

    feat(favorite): implements favorite service and models

---
+++ airsen-frontend/src/app/features/favorites/models/favorite.model.ts
@@ -0,0 +1,7 @@
+export interface Favorite{
+    communeInseeCode: string,
+    communeName: string,
+    departmentName: string,
+    regionName: string,
+    addedAt: Date
+}
---
+++ airsen-frontend/src/app/features/favorites/models/favoriteCheck.model.ts
@@ -0,0 +1,3 @@
+export interface FavoriteCheckResponse {
+    isFavorited: boolean
+}
